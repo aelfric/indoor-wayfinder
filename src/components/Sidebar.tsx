b6f2d838177c0e8de806337c1acf22743fd245c1
Frank Riccobono <F.Riccobono@gmail.com>
2026-05-02 15:31:48 -0400

    Branding

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,6 @@
-import logo from "../assets/img/pathpal-logo.svg";
+import logo from "../assets/img/untitled-design-10.png";
 import { FiChevronRight } from "react-icons/fi";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useMemo } from "react";
 import {
   MapDataContextType,
   NavigationContextType,
@@ -23,13 +23,10 @@ function Sidebar() {
     NavigationContext
   ) as NavigationContextType;
   const { objects } = useContext(MapDataContext) as MapDataContextType;
-  const [parsedObjects, setParsedObjects] = useState<ParsedObjects>({});
-  const [isRotating, setIsRotating] = useState(false);
-  useEffect(() => {
-    const groupedObjects = () => {
+  const parsedObjects = useMemo(()=>{
       const data: ParsedObjects = {};
       objects.forEach((object) => {
-        const firstLetter = object.name.charAt(0).toUpperCase();
+        const firstLetter = String(Math.trunc(Number.parseInt(object.name)  / 100));
         if (!data[firstLetter]) {
           data[firstLetter] = {
             len: 0,
@@ -39,9 +36,7 @@ function Sidebar() {
         data[firstLetter].results.push(object);
         data[firstLetter].len += 1;
       });
-      setParsedObjects(data);
-    };
-    groupedObjects();
+      return data;
   }, [objects]);
 
   function handleObjectNavigation(selectedObjectName: string) {
@@ -56,33 +51,30 @@ function Sidebar() {
     <aside className="flex flex-col rounded-none w-[35rem] h-screen p-3 bg-white shadow-xl shadow-gray-200 -translate-x-full transform transition-transform duration-150 ease-in lg:translate-x-0 lg:shadow-md ">
       <header className="flex flex-col mb-4 pr-1 border-b py-2 w-full">
         <a
-          href="https://github.com/openindoormap/openindoormaps"
+          href="https://github.com/KnotzerIO"
           target="_blank"
           rel="noreferrer"
           className="font-semibold text-blue-800 text-xs"
         >
           <div className="flex items-center p-2 mb-4 text-blue-800 border border-blue-300 rounded-lg bg-blue-50">
             <FaInfoCircle className="w-4 h-4 mr-2" />
-            Check out my new project OpenIndoorMaps
+            This map was developed using software created by Mark Knotzer
           </div>
         </a>
         <div className="flex items-center flex-none mr-10">
-          <div className="rounded-md w-16 h-16 p-4 bg-gray-100 center">
+          <div className="rounded-md w-16 h-16 bg-gray-100 center">
             <img
               src={logo}
               alt="PathPal"
-              className={` ${isRotating ? "rotate" : ""}`}
-              onClick={() => setIsRotating(true)}
-              onAnimationEnd={() => setIsRotating(false)}
             />
           </div>
           <div className="flex flex-col">
             <div className="flex flex-col">
               <p className="text-2xl font-semibold text-gray-900 pl-2">
-                PathPal
+                NCFL 2026
               </p>
               <p className="text-sm font-semibold text-[#225EA9] pl-2">
-                Indoor-Navigation
+                Convention Center Navigation
               </p>
             </div>
           </div>
@@ -90,12 +82,12 @@ function Sidebar() {
       </header>
       <div className="overflow-auto h-full">
         {Object.keys(parsedObjects)
-          .sort()
-          .map((letter, index) => (
-            <div key={index} className="mb-4">
+          .sort((a, b) => Number.parseInt(a) > Number.parseInt(b) ? 1 : -1)
+          .map((letter) => (
+            <div key={letter} className="mb-4">
               <header className="p-2">
                 <h2 className="text-2xl font-bold">
-                  {letter}
+                  {letter}00s
                   <span className="ml-2 text-sm font-medium text-gray-900">
                     - {parsedObjects[letter].len}{" "}
                     {parsedObjects[letter].len === 1 ? "Result" : "Results"}
@@ -104,7 +96,7 @@ function Sidebar() {
               </header>
               <div className="flex flex-col ">
                 {parsedObjects[letter].results.map((item) => (
-                  <div
+                  <button
                     key={item.id?.toString()}
                     data-product={item.name}
                     className="flex bg-[#f4faff] m-1 px-4 py-2 shadow-sm rounded-md cursor-pointer h-auto hover:bg-[#e4f2ff]"
@@ -121,7 +113,7 @@ function Sidebar() {
                     <div className="center ml-auto h-auto center text-xl text-blue-300">
                       <FiChevronRight />
                     </div>
-                  </div>
+                  </button>
                 ))}
               </div>
             </div>
