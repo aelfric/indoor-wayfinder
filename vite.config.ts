6c35829852f4e110dd0c3ba5509f7221bd64101c
Frank Riccobono <F.Riccobono@gmail.com>
2026-05-20 17:04:10 -0400

    No subpath

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 export default defineConfig(({ mode }) => {
   return {
-    base: "http://maps.ncflnationals.org/indoor-wayfinder/",
+    base: "http://maps.ncflnationals.org/",
     plugins: [react(), ViteImageOptimizer({})],
     resolve: {
       alias: {
